98a55f70c8720efb051b6aaa9010b65ca0c74bc1
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-02-22 19:58:18 +0530

    Create Day 21: Generics.cpp

---
+++ Day 21: Generics.cpp
@@ -0,0 +1,23 @@
+Link : https://www.hackerrank.com/challenges/30-generics/problem?isFullScreen=true
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+template<typename Element>
+
+void printArray(vector<Element> arr) {
+    for (int i = 0; i < arr.size(); i++)
+        cout << arr[i] << endl;
+}
+
+int main() {
+    vector<int> vInt{1, 2, 3};
+    vector<string> vString{"Hello", "World"};
+
+    printArray<int>(vInt);
+    printArray<string>(vString);
+
+    return 0;
+}
