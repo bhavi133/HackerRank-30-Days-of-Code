a178c96b6d2818674cf6daa45cc87e269506b0e4
Bhavesh Lulla <65904865+bhavi133@users.noreply.github.com>
2023-02-23 22:51:48 +0530

    Create Day 24: More Linked Lists.py

---
+++ Day 24: More Linked Lists.py
@@ -0,0 +1,48 @@
+Link : https://www.hackerrank.com/challenges/30-linked-list-deletion/problem?isFullScreen=true
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None 
+class Solution: 
+    def insert(self,head,data):
+            p = Node(data)           
+            if head==None:
+                head=p
+            elif head.next==None:
+                head.next=p
+            else:
+                start=head
+                while(start.next!=None):
+                    start=start.next
+                start.next=p
+            return head  
+    def display(self,head):
+        current = head
+        while current:
+            print(current.data,end=' ')
+            current = current.next
+
+    def removeDuplicates(self,head):
+        #Write your code here
+        l = []
+        if head is None:
+            return 
+        else:
+            n = head
+            while n is not None:
+                l.append(n.data)
+                n = n.next
+        nums = sorted(list(set(l)))
+        for i in nums:
+            print(i, end=" ")
+    
+        
+mylist= Solution()
+T=int(input())
+head=None
+for i in range(T):
+    data=int(input())
+    head=mylist.insert(head,data)    
+head=mylist.removeDuplicates(head)
+mylist.display(head); 
